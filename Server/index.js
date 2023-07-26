41a406162f744f90b5f482eca6346beb39f9986e
Manny Delgado <mannygdp@gmail.com>
2023-07-25 22:32:46 -0700

    done

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,5 @@
 
-const express = require('express')
+const express = require('express');
 require('dotenv').config();
 
 const app = express();
@@ -15,17 +15,6 @@ app.get('/', (req, res) => {
     `);
 })
 
-app.get('/teal' , (req, res) => {
-    res.send(` 
-    <body style="margin: 0;">
-        <div style="border: 1px solid black; height: 5vh; background-color: teal;">
-        <h2 style="text-align: center;">NAV BAR</h2>
-        </div>
-        <h1 style="color: teal;">Teal page</h1>
-    </body>
-`);
-})
-
 app.get('/:color' , (req, res) => {
     const myColor = req.params.color;
     res.send(` 
@@ -38,8 +27,7 @@ app.get('/:color' , (req, res) => {
 `);
 })
 
-
 app.listen(process.env.PORT, () => {
-console.log('app is running on port 3000 MD')
+console.log('app is running on port ${process.env.PORT}');
 });
 
